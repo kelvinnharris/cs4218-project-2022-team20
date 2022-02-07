2643d33afa51e4c388f6414a23c9cc931f04e84b
Victor Varian <sacchirro@yahoo.com>
2022-02-07 17:49:49 +0800

    add test folder

---
+++ test/sg/edu/nus/comp/cs4218/impl/app/EchoApplicationTest.java
@@ -0,0 +1,14 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EchoApplicationTest {
+
+    @org.junit.jupiter.api.Test
+    void constructResult() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+    }
+}
---
+++ test/sg/edu/nus/comp/cs4218/impl/app/LsApplicationTest.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+class LsApplicationTest {
+
+    @org.junit.jupiter.api.Test
+    void listFolderContent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+    }
+}
