d63c375f07caec107834cece99b2ff4d6010272a
Evelyne Juliet <evelynejuliet@gmail.com>
2022-02-27 20:49:09 +0800

    remove unused import

--- src/sg/edu/nus/comp/cs4218/impl/app/RmApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/RmApplication.java
@@ -10,8 +10,6 @@ import sg.edu.nus.comp.cs4218.impl.util.IOUtils;
 import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.*;
 import static sg.edu.nus.comp.cs4218.impl.util.ErrorConstants.ERR_NO_PERM;
