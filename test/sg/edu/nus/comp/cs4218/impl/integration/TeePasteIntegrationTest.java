93a8db579f5bf2dc03915ab8cb5b862aef26ee31
Evelyne Juliet <evelynejuliet@gmail.com>
2022-03-14 19:02:47 +0800

    create output files for tee paste integration

--- test/sg/edu/nus/comp/cs4218/impl/integration/TeePasteIntegrationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/integration/TeePasteIntegrationTest.java
@@ -31,8 +31,21 @@ public class TeePasteIntegrationTest {
     public static final String FILE1_PATH = TEST_PATH + FILE1_NAME;
     public static final String FILE2_NAME = "file2.txt";
     public static final String FILE2_PATH = TEST_PATH + FILE2_NAME;
-    public static final String FILE3_NAME = "file3.txt";
-    public static final String FILE3_PATH = TEST_PATH + FILE3_NAME;
+    public static final String OUTPUT1_NAME = "output1.txt";
+    public static final String OUTPUT1_PATH = TEST_PATH + OUTPUT1_NAME;
+    public static final String OUTPUT2_NAME = "output2.txt";
+    public static final String OUTPUT2_PATH = TEST_PATH + OUTPUT2_NAME;
+    public static final String OUTPUT3_NAME = "output3.txt";
+    public static final String OUTPUT3_PATH = TEST_PATH + OUTPUT3_NAME;
+    public static final String OUTPUT4_NAME = "output4.txt";
+    public static final String OUTPUT4_PATH = TEST_PATH + OUTPUT4_NAME;
+    public static final String OUTPUT5_NAME = "output5.txt";
+    public static final String OUTPUT5_PATH = TEST_PATH + OUTPUT5_NAME;
+    public static final String OUTPUT6_NAME = "output6.txt";
+    public static final String OUTPUT6_PATH = TEST_PATH + OUTPUT6_NAME;
+    public static final String OUTPUT7_NAME = "output7.txt";
+    public static final String OUTPUT7_PATH = TEST_PATH + OUTPUT7_NAME;
+
 
     public static final String FIRST = "FIRST";
     public static final String SECOND = "SECOND";
@@ -60,9 +73,21 @@ public class TeePasteIntegrationTest {
         Files.createFile(Paths.get(FILE1_PATH));
         Files.deleteIfExists(Paths.get(FILE2_PATH));
         Files.createFile(Paths.get(FILE2_PATH));
-        Files.deleteIfExists(Paths.get(FILE3_PATH));
-        Files.createFile(Paths.get(FILE3_PATH));
-        appendToFile(Paths.get(FILE3_PATH), LINES);
+        appendToFile(Paths.get(FILE2_PATH), LINES);
+        Files.deleteIfExists(Paths.get(OUTPUT1_PATH));
+        Files.createFile(Paths.get(OUTPUT1_PATH));
+        Files.deleteIfExists(Paths.get(OUTPUT2_PATH));
+        Files.createFile(Paths.get(OUTPUT2_PATH));
+        Files.deleteIfExists(Paths.get(OUTPUT3_PATH));
+        Files.createFile(Paths.get(OUTPUT3_PATH));
+        Files.deleteIfExists(Paths.get(OUTPUT4_PATH));
+        Files.createFile(Paths.get(OUTPUT4_PATH));
+        Files.deleteIfExists(Paths.get(OUTPUT5_PATH));
+        Files.createFile(Paths.get(OUTPUT5_PATH));
+        Files.deleteIfExists(Paths.get(OUTPUT6_PATH));
+        Files.createFile(Paths.get(OUTPUT6_PATH));
+        Files.deleteIfExists(Paths.get(OUTPUT7_PATH));
+        Files.createFile(Paths.get(OUTPUT7_PATH));
     }
 
     @AfterAll
@@ -73,86 +98,86 @@ public class TeePasteIntegrationTest {
 
     @Test
     void testTeePaste_teeFilesAndPaste_shouldReturnCorrectOutput() throws Exception {
-        String commandString = String.format("echo %s | tee %s ; paste %s %s > %s", FROM_TEE, FILE1_PATH, FILE1_PATH, FILE1_PATH, FILE2_PATH);
+        String commandString = String.format("echo %s | tee %s ; paste %s %s > %s", FROM_TEE, FILE1_PATH, FILE1_PATH, FILE1_PATH, OUTPUT1_PATH);
         String expectedStdOut = FROM_TEE + STRING_NEWLINE;
         String expectedContent = FROM_TEE + STRING_TAB +
                 FROM_TEE + STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT1_PATH));
         assertEquals(expectedContent, fileContent);
     }
 
     @Test
     void testTeePaste_teeStdOutOnlyAndPaste_shouldReturnCorrectOutput() throws Exception {
-        String commandString = String.format("echo %s | tee ; paste %s %s > %s", FROM_TEE, FILE1_PATH, FILE1_PATH, FILE2_PATH);
+        String commandString = String.format("echo %s | tee ; paste %s %s > %s", FROM_TEE, FILE1_PATH, FILE1_PATH, OUTPUT2_PATH);
         String expectedStdOut = FROM_TEE + STRING_NEWLINE;
         String expectedContent = STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT2_PATH));
         assertEquals(expectedContent, fileContent);
     }
 
     @Test
     void testTeePaste_teeStdOutOnlyThenPaste_shouldReturnCorrectOutput() throws Exception {
-        String commandString = String.format("echo %s | tee | paste - %s > %s", FROM_TEE, FILE3_PATH, FILE2_PATH);
+        String commandString = String.format("echo %s | tee | paste - %s > %s", FROM_TEE, FILE2_PATH, OUTPUT3_PATH);
         String expectedStdOut = "";
         String expectedContent = FROM_TEE + STRING_TAB +
                 FIRST + STRING_NEWLINE +
                 STRING_TAB + SECOND + STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT3_PATH));
         assertEquals(expectedContent, fileContent);
     }
 
     @Test
     void testTeePaste_pasteThenTeeAppend_shouldReturnCorrectOutput() throws Exception {
-        String commandString = String.format("paste %s %s > %s; echo %s | tee -a %s;", FILE3_PATH, FILE3_PATH, FILE2_PATH, THIRD, FILE2_PATH);
+        String commandString = String.format("paste %s %s > %s; echo %s | tee -a %s;", FILE2_PATH, FILE2_PATH, OUTPUT4_PATH, THIRD, OUTPUT4_PATH);
         String expectedStdOut = THIRD + STRING_NEWLINE;
         String expectedContent = FIRST  + STRING_TAB + FIRST + STRING_NEWLINE +
                 SECOND + STRING_TAB + SECOND + STRING_NEWLINE +
                 THIRD + STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT4_PATH));
         assertEquals(expectedContent, fileContent);
     }
 
     @Test
     void testTeePaste_pasteThenTeeNoAppend_shouldReturnCorrectOutput() throws Exception {
-        String commandString = String.format("paste %s %s > %s; echo %s | tee %s;", FILE3_PATH, FILE3_PATH, FILE2_PATH, THIRD, FILE2_PATH);
+        String commandString = String.format("paste %s %s > %s; echo %s | tee %s;", FILE2_PATH, FILE2_PATH, OUTPUT5_PATH, THIRD, OUTPUT5_PATH);
         String expectedStdOut = THIRD + STRING_NEWLINE;
         String expectedContent = THIRD + STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT5_PATH));
         assertEquals(expectedContent, fileContent);
     }
 
     @Test
     void testTeePaste_pasteWithStdinAsFileThenTeeAppend_shouldReturnCorrectOutput() throws Exception {
-        String commandString = String.format("paste - %s < %s > %s; echo %s | tee -a %s;", FILE3_PATH, FILE3_PATH, FILE2_PATH, THIRD, FILE2_PATH);
+        String commandString = String.format("paste - %s < %s > %s; echo %s | tee -a %s;", FILE2_PATH, FILE2_PATH, OUTPUT6_PATH, THIRD, OUTPUT6_PATH);
         String expectedStdOut = THIRD + STRING_NEWLINE;
         String expectedContent = FIRST  + STRING_TAB + FIRST + STRING_NEWLINE +
                 SECOND + STRING_TAB + SECOND + STRING_NEWLINE +
                 THIRD + STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT6_PATH));
         assertEquals(expectedContent, fileContent);
     }
 
     @Test
     void testTeePaste_pasteErrorThenTeeAppend_shouldThrowError() throws Exception {
-        String commandString = String.format("paste -z; echo %s | tee -a %s;", THIRD, FILE2_PATH);
+        String commandString = String.format("paste -z; echo %s | tee -a %s;", THIRD, OUTPUT7_PATH);
         String expectedStdOut = "paste: invalid option -- 'z'" + STRING_NEWLINE +
                 THIRD + STRING_NEWLINE;
         String expectedContent = THIRD + STRING_NEWLINE;
         shell.parseAndEvaluate(commandString, stdOut);
         assertEquals(expectedStdOut, stdOut.toString());
-        String fileContent = readString(Paths.get(FILE2_PATH));
+        String fileContent = readString(Paths.get(OUTPUT7_PATH));
         assertEquals(expectedContent, fileContent);
     }
 }
