b6baf3992e9bacefbd6478eadaa9a6e42a4d34be
philipalexanderb <philipsenang@gmail.com>
2022-03-18 17:56:25 +0800

    Remove unused import

--- test/sg/edu/nus/comp/cs4218/impl/parser/PasteArgsParserTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/parser/PasteArgsParserTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mockito;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
